924860c7902dda922f9224c2319b874ee943eafd
Chris <roos.chris.p@gmail.com>
2025-05-14 08:07:28 -0400

    s3 file storage app

---
+++ s3project/uploads/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UploadedFile
+from .forms import FileUploadForm
+
+@login_required
+def file_list(request):
+    files = UploadedFile.objects.filter(user=request.user).order_by('-uploaded_at')
+    return render(request, 'uploads/file_list.html', {'files': files})
+
+@login_required
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_instance = form.save(commit=False)
+            file_instance.user = request.user
+            file_instance.save()
+            return redirect('file_list')
+    else:
+        form = FileUploadForm()
+    
+    return render(request, 'uploads/upload_file.html', {'form': form})
