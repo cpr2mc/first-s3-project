addbfd190294ba44ef4809de0a27d19ee0044e8c
Chris <roos.chris.p@gmail.com>
2025-05-18 22:02:05 -0400

    swapped DJANGO_SECRET_KEY and put it in .env

--- s3project/s3project/settings.py
+++ s3project/s3project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t2s0p+4!*!@s_fn06#q57%swh&52-!lgu47%oc(@axzz0i+@b!'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
